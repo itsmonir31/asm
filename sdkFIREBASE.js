9af066c119b55b88cd3147e220d8dac03ca06039
Moniruzzaman <moniruzzaman1931@cseku.ac.bd>
2020-10-16 19:35:12 +0600

    image store in firebase

---
+++ sdkFIREBASE.js
@@ -0,0 +1,24 @@
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyATTxgudFf45YWAd0xBIc5P3wDBHcKODew",
+    authDomain: "demoimg-23b1f.firebaseapp.com",
+    databaseURL: "https://demoimg-23b1f.firebaseio.com",
+    projectId: "demoimg-23b1f",
+    storageBucket: "demoimg-23b1f.appspot.com",
+    messagingSenderId: "671617105734",
+    appId: "1:671617105734:web:1752ed9be2b5e7e80b2f47",
+    measurementId: "G-GW5LLJ088S"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
